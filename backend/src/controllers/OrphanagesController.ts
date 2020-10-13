121e94a6495a48a210bd17592d34ddd7aef7418d
arthursvpb <arthurandroid15@gmail.com>
2020-10-13 14:05:29 -0300

    image upload done

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -19,6 +19,8 @@ export default {
   },
 
   async create(req: Request, res: Response) {
+    // console.log(req.files);
+
     const {
       name,
       latitude,
@@ -30,6 +32,12 @@ export default {
     } = req.body;
 
     const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = req.files as Express.Multer.File[];
+    const images = requestImages.map((image) => {
+      return { path: image.filename };
+    });
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -38,6 +46,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     });
 
     await orphanagesRepository.save(orphanage);
